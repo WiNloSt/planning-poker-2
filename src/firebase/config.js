4ec7527baaaa33f5c94e2abd2f7788d89d3e3ba5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-08-19 18:53:41 +0700

    :tada: Init project using react-static

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyCWW61pkS8yqTTaNqrv7sONWdpzCDS2I8Q',
+  authDomain: 'planning-poker-2.firebaseapp.com',
+  databaseURL: 'https://planning-poker-2.firebaseio.com',
+  projectId: 'planning-poker-2',
+  storageBucket: 'planning-poker-2.appspot.com',
+  messagingSenderId: '175173845161'
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+const db = firebase.database()
+const auth = firebase.auth()
+
+export { db, auth }
