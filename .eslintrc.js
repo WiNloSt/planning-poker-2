1e093edea877813f71624c559b61b6d36ad0c5c4
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-08-25 12:56:50 +0700

    :wrench: Configure ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-  extends: 'react-tools',
+  parser: 'babel-eslint',
+  extends: ['standard', 'standard-react'],
+  env: {
+    jest: true
+  },
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used'
+      }
+    ],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'jsx-quotes': 'off',
+    camelcase: 'off'
+  }
 }
