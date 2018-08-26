6171541c0b3fe971d46c8bef21d6dfff33cfff90
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-08-26 19:02:28 +0700

    :sparkles: Fix Condition compare when using with react-hot-loader

--- src/components/Condition.js
+++ src/components/Condition.js
@@ -1,13 +1,50 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import * as R from 'ramda'
 
-export const Condition = ({ condition, children }) => (
-  <React.Fragment>
-    {React.Children.map(children, child => React.cloneElement(child, { condition }))}
-  </React.Fragment>
-)
+const renderChildren = children => (typeof children === 'function' ? children() : children)
 
-const True = ({ condition, children }) => !!condition && children()
-const False = ({ condition, children }) => !condition && children()
+const getComponentName = element => element.displayName || element.name
+
+const isSameComponent = (a, b) =>
+  process.env.NODE_ENV === 'production' ? a === b : getComponentName(a) === getComponentName(b)
+
+export const Condition = ({ condition, children }) => {
+  let accCondition = true
+  return (
+    <React.Fragment>
+      {React.Children.map(children, child => {
+        if (isSameComponent(child.type, Condition.If)) {
+          const condition = accCondition && child.props.condition
+          accCondition = accCondition && !child.props.condition
+          return React.cloneElement(child, { condition })
+        } else if (
+          isSameComponent(child.type, Condition.True) ||
+          isSameComponent(child.type, Condition.False)
+        ) {
+          return React.cloneElement(child, {
+            condition: !R.isNil(child.props.condition) ? child.props.condition : condition
+          })
+        } else {
+          return child
+        }
+      })}
+    </React.Fragment>
+  )
+}
+Condition.propTypes = {
+  condition: PropTypes.bool,
+  children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]).isRequired
+}
+
+const True = ({ condition, children }) =>
+  !R.isNil(condition) && !!condition && renderChildren(children)
+const False = ({ condition, children }) =>
+  !R.isNil(condition) && !condition && renderChildren(children)
+// Exactly doing like True but we need another cllass for checking in the parent Condition component
+const If = True.bind({})
 
 Condition.True = True
 Condition.False = False
+Condition.If = If
+Condition.ElseIf = If // alias of If
