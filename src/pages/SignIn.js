5d44aecc9a338c77aa5814c3985d84e3f4444f4d
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-09-01 21:58:34 +0700

    :lipstick: Use card component for signin page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Card } from 'antd'
 
 import { GoogleSignInButton } from '../components/GoogleSignInButton'
 import { Consumer } from '../store'
@@ -6,15 +7,17 @@ import { Condition } from '../components/Condition'
 import { Redirect } from 'react-static'
 
 export default () => (
-  <Consumer>
-    {({ authUser, isUserLoaded }) => (
-      <div style={{ textAlign: 'center' }}>
-        <Condition.True condition={authUser}>
-          <Redirect to="/" />
-        </Condition.True>
-        <h1 className="mt0">Login to start</h1>
-        <GoogleSignInButton loading={!isUserLoaded} />
-      </div>
-    )}
-  </Consumer>
+  <Card>
+    <Consumer>
+      {({ authUser, isUserLoaded }) => (
+        <div style={{ textAlign: 'center' }}>
+          <Condition.True condition={authUser}>
+            <Redirect to="/" />
+          </Condition.True>
+          <h1 className="mt0">Login to start</h1>
+          <GoogleSignInButton loading={!isUserLoaded} />
+        </div>
+      )}
+    </Consumer>
+  </Card>
 )
